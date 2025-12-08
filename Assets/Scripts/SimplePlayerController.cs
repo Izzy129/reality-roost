f4e08a7ccf7f7b7bd90b64753766082e5f548a86
Israel Cantu <25886371+Izzy129@users.noreply.github.com>
2025-12-08 13:30:54 -0600

    Sync Relay Test

---
+++ Assets/Scripts/SimplePlayerController.cs
@@ -0,0 +1,33 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class SimplePlayerController : NetworkBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        if (IsOwner)
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+            Debug.Log("You are the green player");
+
+        } else
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+            Debug.Log("You are the red player");
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (!IsOwner) return;
+
+        float speed = 5f;
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        transform.Translate(new Vector3(h, 0, v) * speed * Time.deltaTime);
+        
+    }
+}
