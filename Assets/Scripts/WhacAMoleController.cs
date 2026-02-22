0ec5221e6c6a5ac3e072418b95ece56b60fbba7f
Alex Geels <ag278@rice.edu>
2026-02-22 16:58:43 -0600

    improved whac-a-mole
    added music, ambient nature sounds, feedback sounds.
    
    created a gameplay loop where the player starts by smacking a flower with the mallet, then plays for 1 minute and tries to get a high score

--- Assets/Scripts/WhacAMoleController.cs
+++ Assets/Scripts/WhacAMoleController.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using TMPro;
+
 public class WhacAMoleController : MonoBehaviour
 {
     public MoleController[] moles;
@@ -9,10 +10,15 @@ public class WhacAMoleController : MonoBehaviour
     public int score;
     public TextMeshPro scoreText;
 
+    public GameObject flower, startText;
+    AudioSource audioSource;
+    BoxCollider boxCollider;
+    public AudioClip scoreSound, music;
 
-    void Start()
+    private void Start()
     {
-        StartCoroutine(MoleCoroutine());
+        audioSource = GetComponent<AudioSource>();
+        boxCollider = GetComponent<BoxCollider>();
     }
 
     public void PopMole(MoleController mole, bool up)
@@ -24,22 +30,49 @@ public class WhacAMoleController : MonoBehaviour
 
     IEnumerator MoleCoroutine()
     {
-        while (true)
-        {
-            foreach (var m in moles) {
-                if (!m.isStunned) PopMole(m, Random.value < 0.5f);
+        float endTime = Time.time + 60f;
+        ToggleObjects(true);
+        audioSource.PlayOneShot(music);
 
+        while (Time.time < endTime)
+        {
+            foreach (var m in moles)
+            {
+                if (!m.isStunned)
+                    PopMole(m, Random.value < 0.5f);
             }
 
             yield return new WaitForSeconds(Random.Range(.3f, 1.5f));
         }
+
+        foreach (var m in moles)
+        {
+            PopMole(m, false);
+        }
+
+        ToggleObjects(false);
+    }
+
+    void ToggleObjects(bool gameStarting)
+    {
+        boxCollider.enabled = !gameStarting;
+        flower.SetActive(!gameStarting);
+        startText.SetActive(!gameStarting);
     }
 
     public void IncreaseScore()
     {
+        audioSource.PlayOneShot(scoreSound);
         score++;
         if (scoreText != null)
             scoreText.text = "score: " + score.ToString();
     }
 
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Destroyer"))
+        {
+            StartCoroutine(MoleCoroutine());
+        }
+    }
 }
