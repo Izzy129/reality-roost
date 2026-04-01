3ed2f1557e0981cbec67903f5a06c8c55ff75e01
Alex Geels <ag278@rice.edu>
2026-04-01 18:06:17 -0500

    fixed aruco tracking, duplicated basicscene_colocated
    fixed camera rotation for aruco tracking
    added the basicscene_colocated back since I forgot to earlier

--- Assets/Scripts/TwoMarkerCalibrate.cs
+++ Assets/Scripts/TwoMarkerCalibrate.cs
@@ -1,45 +1,34 @@
 using OpenCVForUnityExample;
-using System.Collections;
-using System.Diagnostics;
 using UnityEngine;
 
 public class TwoMarkerCalibrate : MonoBehaviour
 {
-
     public AruCoMarkerTracker headsetMarker;
-
-    //get the real camera pos prior to running this 
-    public Vector3 realCameraPos;
 
     public GameObject playerRig;
     public GameObject playerCamera;
     public Quaternion rotationOffset;
 
-    public float timeBetweenCalibrationAttempts;
+    public bool calibrateOnlyOnSpace = false;
+    public float smoothSpeed = 5f;
 
-    public float rotScalar;
-    private void Start()
+    private void Update()
     {
-        StartCoroutine(CalibrationCycleCoroutine()); 
-    }
-
-    IEnumerator CalibrationCycleCoroutine()
-    {
-        while (true)
+        if (!calibrateOnlyOnSpace || Input.GetKey(KeyCode.Space))
         {
-            CalibrateWithTwoMarkers();
-            yield return new WaitForSeconds(timeBetweenCalibrationAttempts);
+            CalibrateSmooth();
         }
-
     }
 
-    void CalibrateWithTwoMarkers()
+    void CalibrateSmooth()
     {
-        Vector3 headsetPos = headsetMarker.MarkerPosition;
-        Quaternion headsetRot = headsetMarker.MarkerRotation;
-        UnityEngine.Debug.Log("calibrated. player position: " + headsetPos);
-        playerRig.transform.position = headsetPos;
-        playerCamera.transform.rotation = new Quaternion(headsetRot.x * rotScalar + rotationOffset.x, headsetRot.y * rotScalar + rotationOffset.y, headsetRot.z * rotScalar + rotationOffset.z, 0);
+        Vector3 targetPos = headsetMarker.MarkerPosition;
+        Quaternion targetRot = headsetMarker.MarkerRotation;
 
+        targetPos = new Vector3(-targetPos.x, -targetPos.y, targetPos.z);
+        Quaternion correctedRot = new Quaternion(-targetRot.x, -targetRot.y, targetRot.z, targetRot.w) * rotationOffset;
+
+        playerRig.transform.position = Vector3.Lerp(playerRig.transform.position, targetPos, Time.deltaTime * smoothSpeed);
+        playerCamera.transform.rotation = Quaternion.Slerp(playerCamera.transform.rotation, correctedRot, Time.deltaTime * smoothSpeed);
     }
 }
