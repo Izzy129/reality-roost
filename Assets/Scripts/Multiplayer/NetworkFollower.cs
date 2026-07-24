f82e68d2eb9e43e845e5f9972d49c9d4369a889d
alex4101 <72837778+alex4101@users.noreply.github.com>
2026-07-24 09:48:58 -0500

    Multiplayer: Added Avatar FBT
    Includes ankle trackers

--- Assets/Scripts/Multiplayer/NetworkFollower.cs
+++ Assets/Scripts/Multiplayer/NetworkFollower.cs
@@ -3,15 +3,22 @@ using UnityEngine;
 
 public class NetworkFollower : NetworkBehaviour
 {
+    [SerializeField] private bool _isInFBT = false;
     // Prefab references
     public GameObject head;
     public GameObject leftController;
     public GameObject rightController;
+    public GameObject leftAnkle;
+    public GameObject rightAnkle;
+    //public GameObject waist;
 
     // Scene references
-    [SerializeField] private GameObject headRef;
-    [SerializeField] private GameObject leftControllerRef;
-    [SerializeField] private GameObject rightControllerRef;
+    private GameObject headRef;
+    private GameObject leftControllerRef;
+    private GameObject rightControllerRef;
+    private GameObject leftAnkleRef;
+    private GameObject rightAnkleRef;
+    //private GameObject waistRef;
 
     private void Start()
     {
@@ -20,6 +27,13 @@ public class NetworkFollower : NetworkBehaviour
             headRef = GameObject.Find("Head Target");
             leftControllerRef = GameObject.Find("Left Hand Target");
             rightControllerRef = GameObject.Find("Right Hand Target");
+
+            if(_isInFBT)
+            {
+                leftAnkleRef = GameObject.Find("Left Ankle Target");
+                rightAnkleRef = GameObject.Find("Right Ankle Target");
+                //waistRef = GameObject.Find("Waist Target");
+            }
         }
     }
 
@@ -30,6 +44,13 @@ public class NetworkFollower : NetworkBehaviour
             FollowTransform(head, headRef);
             FollowTransform(leftController, leftControllerRef);
             FollowTransform(rightController, rightControllerRef);
+            
+            if (_isInFBT)
+            {
+                FollowTransform(leftAnkle, leftAnkleRef);
+                FollowTransform (rightAnkle, rightAnkleRef);
+                //FollowTransform(waist, waistRef);
+            }
         }
     }
 
