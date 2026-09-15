c716ea519b641392fb0bb1426bd1f45349c566bb
Alexandra Barrett <72837778+alex4101@users.noreply.github.com>
2026-09-15 14:06:32 -0500

    v0.3.1: Fixed source file path

--- Packages/net.xpixels.reality-roost/Editor/RRBuildProfileImporter.cs
+++ Packages/net.xpixels.reality-roost/Editor/RRBuildProfileImporter.cs
@@ -7,7 +7,7 @@ namespace RealityRoost.Editor
     {
 #if UNITY_6000_6_OR_NEWER
 
-        const string Source = "Packages/Reality Roost SDK/Editor/Build Profiles/RR-Host.asset";
+        const string Source = "Packages/net.xpixels.reality-roost/Editor/Build Profiles/RR-Host.asset";
         const string Destination = "Assets/Settings/Build Profiles/RR-Host.asset";
 
         [MenuItem("Reality Roost/Add Build Profile", priority = 99)]
