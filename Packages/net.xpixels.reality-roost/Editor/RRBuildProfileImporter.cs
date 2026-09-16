f00cac830573fd4628c20f61a6281809a905418f
Alexandra Barrett <72837778+alex4101@users.noreply.github.com>
2026-09-16 12:09:18 -0500

    SDK v0.3.5: 6000.0 now has 'Add build profile' dropdown
    In case 6000.0 has issue where Build Profiles has no scenes listed, this is a backup option to ensure functionality

--- Packages/net.xpixels.reality-roost/Editor/RRBuildProfileImporter.cs
+++ Packages/net.xpixels.reality-roost/Editor/RRBuildProfileImporter.cs
@@ -6,6 +6,62 @@ namespace RealityRoost.Editor
 {
     public static class RRBuildProfileImporter
     {
+#if UNITY_6000_0
+        // Add scenes to build profile
+        [MenuItem("Reality Roost/Add Build Profile", priority = 99)]
+        public static void AddScenesToBuildProfile()
+        {
+            var source = "Packages/net.xpixels.reality-roost/Editor/Build Profiles/RR-Host.asset";
+
+            string[] bootGuids = AssetDatabase.FindAssets(
+         "RR_Boot t:Scene",
+         new[] { "Assets/Samples/Reality Roost SDK" }
+     );
+
+            string[] calibGuids = AssetDatabase.FindAssets(
+                "RR_Calib t:Scene",
+                new[] { "Assets/Samples/Reality Roost SDK" }
+            );
+
+            // Check that Required Boot Scenes sample was imported
+            if (bootGuids.Length == 0 || calibGuids.Length == 0)
+            {
+                EditorUtility.DisplayDialog(
+                    "Required Boot Scenes Missing",
+                    "Please import the 'Required Boot Scenes' sample before adding the RR-Host Build Profile.",
+                    "OK"
+    );
+                return;
+            }
+
+            string rrBootScenePath =
+                AssetDatabase.GUIDToAssetPath(bootGuids[0]);
+
+            string rrCalibScenePath =
+                AssetDatabase.GUIDToAssetPath(calibGuids[0]);
+
+            var profile =
+                AssetDatabase.LoadAssetAtPath<UnityEditor.Build.Profile.BuildProfile>(
+                    source
+                );
+
+            if (profile == null)
+            {
+                Debug.LogError("RR-Host Build Profile could not be loaded.");
+                return;
+            }
+
+            profile.scenes = new[]
+            {
+        new EditorBuildSettingsScene(rrBootScenePath, true),
+        new EditorBuildSettingsScene(rrCalibScenePath, true)
+    };
+
+            EditorUtility.SetDirty(profile);
+            AssetDatabase.SaveAssetIfDirty(profile);
+
+            Debug.Log("RR-Host scenes added successfully.");
+#endif
 #if UNITY_6000_6_OR_NEWER
 
         const string Source = "Packages/net.xpixels.reality-roost/Editor/Build Profiles/RR-Host.asset";
@@ -94,5 +150,6 @@ namespace RealityRoost.Editor
             Debug.Log("RR-Host scenes added successfully.");
         }
 #endif
+        }
     }
 }
